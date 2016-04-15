c50b74c21aae74ff03bdea53eae49a50bb962b44
Trevor Williams <whiltx@gmail.com>
2016-04-15 15:14:12 -0400

    Further initialization work.

--- js/game.js
+++ js/game.js
@@ -1,18 +1,10 @@
 
 /* Game namespace */
 var game = {
-
-    // an object where to store game information
-    data : {
-        // score
-        score : 0
-    },
-
-
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -39,12 +31,11 @@ var game = {
 
     // Run on game resources loaded.
     "loaded" : function () {
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+        // set the "Play/Ingame" Screen Object
+        this.playScreen = new game.PlayScreen();
+        me.state.set(me.state.PLAY, this.playScreen);
 
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
-
-        // Start the game.
+        // start the game
         me.state.change(me.state.PLAY);
     }
 };
